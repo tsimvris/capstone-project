0d2eecc2007abe06f2774dedfcb99f30989a4fcf
Petros Tsimvris <tsimvris@gmail.com>
2022-08-15 11:03:58 +0200

    created styled form

---
+++ src/components/Forms/styledForm.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const StyledForm = styled.form`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	width: 330px;
+	height: 500px;
+	margin-bottom: 10px;
+	padding: 10px;
+	border-radius: 20px;
+	background-color: #19647e;
+`;
+
+export default StyledForm;
