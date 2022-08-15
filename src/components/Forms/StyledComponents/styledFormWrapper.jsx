0dfa2c5ba1e6a57f1e1c96441cbabe1080b65d9e
Petros Tsimvris <tsimvris@gmail.com>
2022-08-15 11:08:52 +0200

    created form wrapper

---
+++ src/components/Forms/StyledComponents/styledFormWrapper.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 375px;
+	height: 667px;
+`;
+
+export default StyledWrapper;
