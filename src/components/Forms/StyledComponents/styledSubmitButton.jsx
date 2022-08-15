80f06754e0fe43ccf70b5add090389e3d0bb104d
Petros Tsimvris <tsimvris@gmail.com>
2022-08-15 11:07:53 +0200

    added styled button

---
+++ src/components/Forms/StyledComponents/styledSubmitButton.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const StyledSubmitButton = styled.button`
+	width: 238px;
+	height: 47px;
+	margin: 10px auto;
+	border-radius: 13px;
+	background-color: #fc814a;
+	color: #19647e;
+	font-size: 24px;
+	font-weight: 800;
+`;
+
+export default StyledSubmitButton;
