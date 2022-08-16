a73ab442100b8a4ee1ad060a832cb5ba00ce6427
Petros Tsimvris <tsimvris@gmail.com>
2022-08-16 11:36:45 +0200

    deleted default store, added name and storage an store

--- src/hooks/useClientStore.js
+++ src/hooks/useClientStore.js
@@ -2,14 +2,20 @@ import create from 'zustand';
 import {persist} from 'zustand/middleware';
 
 const useClientStore = create(
-	persist(set => ({
-		clients: [],
-		addClient: client => {
-			set(state => {
-				return {clients: [...state.clients, client]};
-			});
-		},
-	}))
+	persist(
+		set => ({
+			clients: [],
+			addClient: client => {
+				set(state => {
+					return {clients: [...state.clients, client]};
+				});
+			},
+		}),
+		{
+			name: 'Clients', // unique name
+			getStorage: () => localStorage, // (optional) by default, 'localStorage' is used
+		}
+	)
 );
 
 export default useClientStore;
