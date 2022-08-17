7a1c8492813d75a77e85a3d6e3c834f6c972d9b1
Tsimvris <101249132+tsimvris@users.noreply.github.com>
2022-08-17 18:29:17 +0200

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,7 @@
 import dynamic from 'next/dynamic';
-
 import Layout from '../components/Layout';
+
 export default function Homepage() {
-	const router = useRouter();
 	const DynamicWrapper = dynamic(
 		() => import('../components/Forms/StyledComponents/styledFormWrapper'),
 		{
