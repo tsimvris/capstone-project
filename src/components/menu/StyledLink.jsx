a9fafb7ac07e2a01a94d77d1c1ea4df9f64203e9
Petros Tsimvris <tsimvris@gmail.com>
2022-08-17 18:22:45 +0200

    finished Drawer Menu

---
+++ src/components/menu/StyledLink.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StyledLink = styled.a`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 130px;
+	height: 55px;
+	margin-bottom: 42px;
+	border-radius: 25px;
+	background-color: #fff;
+	color: #19647e;
+`;
+
+export default StyledLink;
---
+++ src/components/menu/styledMenuBox.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const StyledMenuBox = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: flex-start;
+	width: 150px;
+	height: 100%;
+	background-color: #19647e;
+`;
+
+export default StyledMenuBox;
