9840d0e0896e0914e7f0d5ebfc8fbf7773349a85
Petros Tsimvris <tsimvris@gmail.com>
2022-08-21 12:20:46 +0200

    search funktion working but only with 1:1 search

--- src/pages/clients/clients.jsx
+++ src/pages/clients/clients.jsx
@@ -1,12 +1,15 @@
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 import {useRouter} from 'next/router';
+import {useState} from 'react';
+import {useRef} from 'react';
 
 import StyledLi from '../../components/ClientUI/styledLi';
 import StyledSearchField from '../../components/ClientUI/styledSearchField';
 import StyledSpan from '../../components/ClientUI/styledSpan';
 import StyledUl from '../../components/ClientUI/styledUL';
 import StyledEditButton from '../../components/Forms/StyledComponents/styledEditButton';
+import StyledSubmitButton from '../../components/Forms/StyledComponents/StyledSubmitButton';
 import Layout from '../../components/Layout';
 import StyledButton from '../../components/styledButton';
 import StyledWrapper from '../../components/styledClientWrapper';
@@ -15,9 +18,23 @@ import useClientStore from '../../hooks/useClientStore';
 export default function ClientsPage() {
 	const clients = useClientStore(state => state.clients);
 	const router = useRouter();
+	const inputRef = useRef(null);
 	const DynamicWrapper = dynamic(() => import('../../components/styledClientWrapper'), {
 		ssr: false,
 	});
+
+	// search function
+	const [filteredClients, setFilteredClients] = useState(clients);
+	const clientSearch = inputRef => {
+		const filtered = clients.filter(client => {
+			return client.CompanyName === inputRef.current.value;
+		});
+		setFilteredClients(filtered);
+	};
+	function handleClick() {
+		console.log(inputRef.current.value);
+		console.log(filteredClients);
+	}
 	return (
 		<Layout>
 			<Head>
@@ -26,8 +43,48 @@ export default function ClientsPage() {
 				<link rel="icon" href="/Dashy.webp" />
 			</Head>
 			<DynamicWrapper>
-				<StyledSearchField placeholder="Search for a Client" />
-				<p></p>
+				<form
+					className="form"
+					onSubmit={event => {
+						event.preventDefault();
+						clientSearch(inputRef);
+					}}
+				>
+					<StyledSearchField
+						type="search"
+						ref={inputRef}
+						placeholder="Search for a Client"
+					/>
+					<StyledSubmitButton onClick={handleClick} name="submitButton" type="submit">
+						Search
+					</StyledSubmitButton>
+				</form>
+
+				<StyledUl>
+					{filteredClients ? (
+						<>
+							{filteredClients.map(client => {
+								return (
+									<StyledLi key={client.id}>
+										<StyledSpan>{client.CompanyName}</StyledSpan>
+										<StyledEditButton
+											onClick={() => {
+												router.push({
+													pathname: `/clients/${client.id}`,
+													query: {keyword: 'clientId'},
+												});
+											}}
+										>
+											Edit
+										</StyledEditButton>
+									</StyledLi>
+								);
+							})}
+						</>
+					) : (
+						''
+					)}
+				</StyledUl>
 				<StyledButton
 					onClick={() => {
 						router.push({
