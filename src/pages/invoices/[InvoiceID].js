e465f1562835bd5450c2bf74ac4f610927564f01
Petros Tsimvris <tsimvris@gmail.com>
2022-08-23 14:12:23 +0200

    finished pdf invoice

--- src/pages/invoices/[InvoiceID].js
+++ src/pages/invoices/[InvoiceID].js
@@ -7,7 +7,6 @@ import StyledSubmitButton from '../../components/Forms/StyledComponents/styledSu
 import useClientStore from '../../hooks/useClientStore';
 import useMyStore from '../../hooks/useMyStore';
 
-import StyledClientDiv from './StyledClientDiv';
 import StyledCompanyDiv from './styledCompanyDiv';
 
 const styles = StyleSheet.create({
@@ -38,6 +37,78 @@ const styles = StyleSheet.create({
 		fontWeight: 800,
 		fontSize: 20,
 		marginBottom: '10px',
+		padding: '10px',
+	},
+	invoiceInfo: {
+		fontWeight: 700,
+		fontSize: 16,
+		marginBottom: '10px',
+	},
+	secondRow: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	invoiceInfoContainer: {
+		marginTop: '40px',
+		marginRight: '20px',
+	},
+	invoiceTotal: {
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: '100%',
+		fontWeight: 700,
+	},
+	InvoiceTotalWrapper: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		width: '100%',
+	},
+	highlight: {
+		fontWeight: 800,
+		fontSize: 20,
+		paddingLeft: '10px',
+		paddingRight: '10px',
+	},
+	serviceWrapper: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	serviceUnterWrapper: {
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	subtotal: {
+		width: '100%',
+		display: 'flex',
+		justifyContent: 'flex-end',
+		alignItems: 'flex-end',
+		marginRight: '20px',
+		marginTop: '20px',
+		paddingRight: '10px',
+	},
+	taxes: {
+		display: 'flex',
+		marginBottom: '20px',
+		marginRight: '20px',
+		marginTop: '10px',
+		paddingRight: '10px',
+	},
+	subInfo: {
+		display: 'flex',
+		width: '100%',
+		height: 'auto',
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+		marginTop: '10px',
 	},
 });
 export default function MyDocument() {
@@ -88,7 +159,7 @@ export default function MyDocument() {
 								<Text style={styles.text}>{myCompanyInfo[0]?.myCompanyCity}</Text>
 							</View>
 						</StyledCompanyDiv>
-						<StyledClientDiv>
+						<View style={styles.secondRow}>
 							<View style={styles.myCompany} aria="Client Data">
 								<Text style={styles.strong}>BILL TO</Text>
 								<Text style={styles.text}>{wantedClient[0]?.CompanyName}</Text>
@@ -96,7 +167,76 @@ export default function MyDocument() {
 								<Text style={styles.text}>{wantedClient[0]?.CompanyZipCode} </Text>
 								<Text style={styles.text}>{wantedClient[0]?.CompanyCity}</Text>
 							</View>
-						</StyledClientDiv>
+							<View
+								style={styles.invoiceInfoContainer}
+								aria="Invoice Date and Number"
+							>
+								<Text style={styles.text}>
+									<Text style={styles.invoiceInfo}>Invoice ID : </Text>
+									{wantedInvoice?.id}
+								</Text>
+								<Text style={styles.text}>
+									<Text style={styles.invoiceInfo}>Invoice Date : </Text>
+									{wantedInvoice?.invoiceDate}
+								</Text>
+								<Text style={styles.text}>
+									<Text style={styles.invoiceInfo}>Due Date : </Text>
+									{wantedInvoice?.invoiceDueDate}
+								</Text>
+							</View>
+						</View>
+						<Text style={styles.strong}>
+							--------------------------------------------------------------------------------------------
+						</Text>
+						<View style={styles.serviceWrapper} aria="Invoice Services">
+							<View style={styles.serviceUnterWrapper} aria="service description">
+								<Text style={styles.strong}>Service Description</Text>
+								<Text>{wantedInvoice?.invoiceService}</Text>
+							</View>
+							<View style={styles.serviceUnterWrapper} aria="Price / Hour">
+								<Text style={styles.strong}>Hourly Rate</Text>
+								<Text>{wantedInvoice?.invoicePriceHour} €</Text>
+							</View>
+							<View style={styles.serviceUnterWrapper} aria="Subtotal">
+								<Text style={styles.strong}>Amount</Text>
+								<Text>{wantedInvoice?.invoiceSubtotal} €</Text>
+							</View>
+						</View>
+						<Text style={styles.strong}>
+							--------------------------------------------------------------------------------------------
+						</Text>
+						<View style={styles.subtotal} aria="Invoice Subtotal and taxes">
+							<Text style={styles.taxes}>
+								Subtotal :<Text>{wantedInvoice?.invoiceSubtotal} €</Text>
+							</Text>
+							<Text style={styles.taxes}>
+								Sales Tax {wantedInvoice?.invoiceTaxKey}% :
+								<Text>{wantedInvoice?.invoiceTaxes} €</Text>
+							</Text>
+						</View>
+						<View style={styles.invoiceTotal} aria="Invoice Total">
+							<Text style={styles.strong}>
+								--------------------------------------------------------------------------------------------
+							</Text>
+							<View style={styles.InvoiceTotalWrapper}>
+								<Text style={styles.highlight}>Invoice Total : </Text>
+								<Text style={styles.highlight}>
+									{wantedInvoice?.invoiceTotal} €
+								</Text>
+							</View>
+
+							<Text style={styles.strong}>
+								--------------------------------------------------------------------------------------------
+							</Text>
+						</View>
+						<View style={styles.subInfo} aria="Terms and Conditions">
+							<Text>{wantedInvoice?.invoicePaymentDue}</Text>
+							<Text>Bank : {wantedInvoice?.invoiceBank}</Text>
+							<Text>IBAN : {wantedInvoice?.invoiceIban}</Text>
+							<Text>
+								Payment Reference : {wantedInvoice?.invoicePaymentReference}
+							</Text>
+						</View>
 					</Page>
 				</Document>
 			</PDFViewer>
--- src/pages/invoices/styledClientDiv.jsx
+++ src/pages/invoices/styledClientDiv.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 const StyledClientDiv = styled.div`
 	display: flex;
-	flex-direction: column;
+	flex-direction: row;
 	align-items: flex-start;
 	justify-content: flex-start;
 	width: 100vw;
